3185b19409e3c974c5b7f0e455c1de2f213a97c1
Victor Hugo Batista <vct.bta@gmail.com>
2015-04-29 10:12:13 -0300

    Correção dos bugs relatados por mim

--- js/wirewolfCustom.js
+++ js/wirewolfCustom.js
@@ -1,14 +1,3 @@
-			addEventListener(
-				"load",
-				function(){
-					setTimeout(hideURLbar, 0);
-				},
-				false);				
-			function hideURLbar(){
-				window.scrollTo(0,1);
-			}
-				
-			
 			jQuery(document).ready(function($) {
 				$(".scroll").click(function(event){		
 					event.preventDefault();
